0152eda5e0fe7945e4a4829466852b395fb280c3
d0dge1003 <ignaciokarelearl@gmail.com>
2025-09-03 14:00:14 +0800

    wo1 home page assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('year').textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.getElementById('lastmodified').textContent = `Last Modified: ${lastModified}`;
